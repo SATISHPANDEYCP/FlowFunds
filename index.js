3868299d665ef11b9168e5b4043c511e23ba7b01
singhmanish07 <manish13singh19@gmail.com>
2025-02-08 16:17:03 +0530

    login

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+root
+
+export default index
---
+++ view/login-signup.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div className='login'>
+        <div>
+            <input type="Name:" placeholder='Name' />
+            <input type="Email:" placeholder='email'/>
+            <input type="Password:" placeholder='password' />
+        </div>
+      
+    </div>
+  )
+}
+
+export default login
